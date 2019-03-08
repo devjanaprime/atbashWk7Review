a35cb25c3835d74644766c59d8053fcbf1574918
Dev Jana <dev@primeacademy.io>
2019-03-08 14:41:39 -0600

    inventory additions working

--- client.js
+++ client.js
@@ -1,5 +1,17 @@
 $( document ).ready( readyNow );
+let inventory = [];
+
+function addItem(){
+    console.log( 'in addItem' );
+    let newItem = {
+        name: $( '#itemNameIn' ).val(),
+        size: $( '#sizeIn' ).val(),
+        color: $( '#colorIn' ).val()
+    } // end newItem
+    inventory.push( newItem );
+    console.log( 'inventory:', inventory );
+} // end addItem
 
 function readyNow(){
-    console.log( 'in readyNow' );
+    $( '#addItemButton' ).on( 'click', addItem )
 } // end readyNow
